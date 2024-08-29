6b0ae6b9f91333c45b1e9d87071ee37fe9ddb9cd
Tyler Prellwitz <tylerprellwitz@Tylers-MacBook-Air.local>
2024-08-28 17:17:52 -0700

    area and perimeter 2.3 added

---
+++ Unit2/areaAndPerimeter.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int length = 8;
+    int width = 3;
+
+    cout << "The area of the rectangle is " << (length * width) << endl;
+    cout << "The perimeter of the rectangle is " << (2*length + 2*width) << endl;
+
+}
